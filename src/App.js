b0b1c2e80b151d2c3fbc0f6049e17a61051438b3
Dago R <dagoramirezserna@gmail.com>
2020-10-19 18:30:03 -0500

    Se agrega tabla

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import CustomizedTables from './components/CustomizedTables'
 
 function App() {
   return (
     <div className="App">
+      <CustomizedTables />
       <Button variant="contained" color="primary">
         Primary
       </Button>
